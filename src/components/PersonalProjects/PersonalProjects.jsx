94a0ec32d43f13f60538ee4a333e84961e71e651
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-08-21 21:31:25 +0530

    project section added

--- src/components/PersonalProjects/PersonalProjects.jsx
+++ src/components/PersonalProjects/PersonalProjects.jsx
@@ -8,7 +8,7 @@ import {
 export default function PersonalProject() {
   const [selectedProject, setSelectedProject] = useState(null);
   return (
-    <section className="Hero container m-auto px-[200px] py-[50px]">
+    <section className="Hero container m-auto `px-[200px] py-[50px]`">
       <h2 className="text-white font-bold text-3xl mb-10">Personal Projects</h2>
 
       <div className="grid grid-cols-3 gap-5">
