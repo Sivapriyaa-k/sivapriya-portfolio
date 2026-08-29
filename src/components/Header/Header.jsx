9b94c9e62c64c380685d7060141b7e2cf9daa0a1
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2026-08-29 11:24:35 +0530

    updated header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,10 +12,16 @@ export default function Header() {
             >
               SP
             </a>
-            <div className="nav-links flex items-center justify-center gap-10 text-[#A1A1A1] hover:text-[#EE8A25] font-semibold">
-              <a href="/">Home</a>
-              <a href="#projects">Projects</a>
-              <a href="#contact">Contact</a>
+            <div className="nav-links flex items-center justify-center gap-10 text-[#A1A1A1]font-semibold">
+              <a href="/" className=" hover:text-[#EE8A25] ">
+                Home
+              </a>
+              <a href="#projects" className=" hover:text-[#EE8A25] ">
+                Projects
+              </a>
+              <a href="#contact" className=" hover:text-[#EE8A25] ">
+                Contact
+              </a>
             </div>
           </div>
         </nav>
